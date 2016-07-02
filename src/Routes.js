6c622e1940f05b5bb440ec93540b7313ed6064b7
Naoki Iwata <a.tokai.wain@gmail.com>
2016-07-02 18:51:09 +0900

    React.jsの雛形を追加

---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Route,
+  Redirect,
+  IndexRoute,
+} from 'react-router';
+
+import Eat from './pages/Eat';
+import Fun from './pages/Fun';
+import Stay from './pages/Stay';
+import Emergency from './pages/Emergency';
+import Master from './pages/Master';
+
+const AppRoutes = (
+  <Route path="/" component={Master}>
+    <Route path="eat/:query" component={Eat}/>
+    <Route path="fun" component={Fun}/>
+    <Route path="stay" component={Stay}/>
+    <Route path="emergency" component={Emergency}/>
+  </Route>
+);
+
+export default AppRoutes;
