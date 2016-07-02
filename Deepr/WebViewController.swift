b17fca29336b78841ce41f14cdaa411ebaeff723
Ikuo Degawa <degawa@ikuwow.com>
2016-07-02 16:43:56 +0900

    Add basic webview

---
+++ Deepr/WebViewController.swift
@@ -0,0 +1,40 @@
+//
+//  WebViewController.swift
+//  Deepr
+//
+//  Created by DegawaIkuo on 7/2/16.
+//  Copyright © 2016 ikuwow. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let requestURL = NSURL(string: "https://ikuwow.github.io")
+        let req = NSURLRequest(URL: requestURL!)
+        webView.loadRequest(req)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
